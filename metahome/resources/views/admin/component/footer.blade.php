ced685c6c9b8829b3e3adf77199893b1b67a3da5
Nguyen Ngoc Duc <nguyenngocduc.tosa@gmail.com>
2024-10-30 09:00:04 +0700

    new

---
+++ metahome/resources/views/admin/component/footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container-fluid d-flex justify-content-center">
+        <div class="copyright">
+            <?php echo date('Y'); ?>, made with <i class="fa fa-heart heart text-danger"></i> by
+            <a href="https://www.facebook.com/Duckynocirclek/">Ducky</a>
+            . Distributed by
+            <a target="_blank" href="https://metasoftware.vn/">MetaSoft</a>.
+        </div>
+    </div>
+</footer>
