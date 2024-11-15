11612eb2372b02b4e450b5f9ee135556438c8580
Nguyen Ngoc Duc <nguyenngocduc.tosa@gmail.com>
2024-11-15 09:55:36 +0700

    mmm

--- metahome/resources/views/admin/index.blade.php
+++ metahome/resources/views/admin/index.blade.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
+
+<head>
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <title>METAHOME - Admin Dashboard</title>
-    <meta
-      content="width=device-width, initial-scale=1.0, shrink-to-fit=no"
-      name="viewport"
-    />
+    <meta content="width=device-width, initial-scale=1.0, shrink-to-fit=no" name="viewport" />
     <link rel="apple-touch-icon" sizes="57x57" href="images/favicon/apple-icon-57x57.png">
     <link rel="apple-touch-icon" sizes="60x60" href="images/favicon/apple-icon-60x60.png">
     <link rel="apple-touch-icon" sizes="72x72" href="images/favicon/apple-icon-72x72.png">
@@ -16,7 +14,7 @@
     <link rel="apple-touch-icon" sizes="144x144" href="images/favicon/apple-icon-144x144.png">
     <link rel="apple-touch-icon" sizes="152x152" href="images/favicon/apple-icon-152x152.png">
     <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-icon-180x180.png">
-    <link rel="icon" type="image/png" sizes="192x192"  href="images/favicon/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="images/favicon/android-icon-192x192.png">
     <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="96x96" href="images/favicon/favicon-96x96.png">
     <link rel="icon" type="image/png" sizes="16x16" href="images/favicon/favicon-16x16.png">
@@ -27,22 +25,26 @@
 
     <!-- Fonts and icons -->
     <script src="assets/js/plugin/webfont/webfont.min.js"></script>
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.css">
     <script>
-      WebFont.load({
-        google: { families: ["Public Sans:300,400,500,600,700"] },
-        custom: {
-          families: [
-            "Font Awesome 5 Solid",
-            "Font Awesome 5 Regular",
-            "Font Awesome 5 Brands",
-            "simple-line-icons",
-          ],
-          urls: ["assets/css/fonts.min.css"],
-        },
-        active: function () {
-          sessionStorage.fonts = true;
-        },
-      });
+        WebFont.load({
+            google: {
+                families: ["Public Sans:300,400,500,600,700"]
+            },
+            custom: {
+                families: [
+                    "Font Awesome 5 Solid",
+                    "Font Awesome 5 Regular",
+                    "Font Awesome 5 Brands",
+                    "simple-line-icons",
+                ],
+                urls: ["assets/css/fonts.min.css"],
+            },
+            active: function() {
+                sessionStorage.fonts = true;
+            },
+        });
     </script>
 
     <!-- CSS Files -->
@@ -50,20 +52,22 @@
     <link rel="stylesheet" href="assets/css/plugins.min.css" />
     <link rel="stylesheet" href="assets/css/kaiadmin.min.css" />
 
+
     <!-- CSS Just for demo purpose, don't include it in your project -->
     <link rel="stylesheet" href="assets/css/demo.css" />
-  </head>
-  <body>
-    <div class="wrapper">
-      <!-- Sidebar -->
-      @include('admin.component.sidebar')
-      <!-- End Sidebar -->
+</head>
 
-      <div class="main-panel">
-        @include('admin.component.header')
-        @yield('body')
-        @include('admin.component.footer')
-      </div>
+<body>
+    <div class="wrapper">
+        <!-- Sidebar -->
+        @include('admin.component.sidebar')
+        <!-- End Sidebar -->
+
+        <div class="main-panel">
+            @include('admin.component.header')
+            @yield('body')
+            @include('admin.component.footer')
+        </div>
 
     </div>
 
@@ -101,63 +105,103 @@
     <!-- Kaiadmin JS -->
     <script src="assets/js/kaiadmin.min.js"></script>
 
+    <!-- Croppie -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/croppie/2.6.5/croppie.min.js"
+        integrity="sha512-Gs+PsXsGkmr+15rqObPJbenQ2wB3qYvTHuJO6YJzPe/dTLvhy0fmae2BcnaozxDo5iaF8emzmCZWbQ1XXiX2Ig=="
+        crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
     <script>
-        $(document).ready(function () {
-          $("#basic-datatables").DataTable({});
+        $(document).ready(function() {
+            $("#basic-datatables").DataTable({});
 
-          $("#multi-filter-select").DataTable({
-            pageLength: 5,
-            initComplete: function () {
-              this.api()
-                .columns()
-                .every(function () {
-                  var column = this;
-                  var select = $(
-                    '<select class="form-select"><option value=""></option></select>'
-                  )
-                    .appendTo($(column.footer()).empty())
-                    .on("change", function () {
-                      var val = $.fn.dataTable.util.escapeRegex($(this).val());
+            $("#multi-filter-select").DataTable({
+                pageLength: 5,
+                initComplete: function() {
+                    this.api()
+                        .columns()
+                        .every(function() {
+                            var column = this;
+                            var select = $(
+                                    '<select class="form-select"><option value=""></option></select>'
+                                )
+                                .appendTo($(column.footer()).empty())
+                                .on("change", function() {
+                                    var val = $.fn.dataTable.util.escapeRegex($(this).val());
 
-                      column
-                        .search(val ? "^" + val + "$" : "", true, false)
-                        .draw();
-                    });
+                                    column
+                                        .search(val ? "^" + val + "$" : "", true, false)
+                                        .draw();
+                                });
 
-                  column
-                    .data()
-                    .unique()
-                    .sort()
-                    .each(function (d, j) {
-                      select.append(
-                        '<option value="' + d + '">' + d + "</option>"
-                      );
-                    });
-                });
-            },
-          });
+                            column
+                                .data()
+                                .unique()
+                                .sort()
+                                .each(function(d, j) {
+                                    select.append(
+                                        '<option value="' + d + '">' + d + "</option>"
+                                    );
+                                });
+                        });
+                },
+            });
 
-          // Add Row
-          $("#add-row").DataTable({
-            pageLength: 5,
-          });
+            // Add Row
+            $("#add-row").DataTable({
+                pageLength: 5,
+            });
 
-          var action =
-            '<td> <div class="form-button-action"> <button type="button" data-bs-toggle="tooltip" title="" class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task"> <i class="fa fa-edit"></i> </button> <button type="button" data-bs-toggle="tooltip" title="" class="btn btn-link btn-danger" data-original-title="Remove"> <i class="fa fa-times"></i> </button> </div> </td>';
+            var action =
+                '<td> <div class="form-button-action"> <button type="button" data-bs-toggle="tooltip" title="" class="btn btn-link btn-primary btn-lg" data-original-title="Edit Task"> <i class="fa fa-edit"></i> </button> <button type="button" data-bs-toggle="tooltip" title="" class="btn btn-link btn-danger" data-original-title="Remove"> <i class="fa fa-times"></i> </button> </div> </td>';
 
-          $("#addRowButton").click(function () {
-            $("#add-row")
-              .dataTable()
-              .fnAddData([
-                $("#addName").val(),
-                $("#addPosition").val(),
-                $("#addOffice").val(),
-                action,
-              ]);
-            $("#addRowModal").modal("hide");
-          });
+            $("#addRowButton").click(function() {
+                $("#add-row")
+                    .dataTable()
+                    .fnAddData([
+                        $("#addName").val(),
+                        $("#addPosition").val(),
+                        $("#addOffice").val(),
+                        action,
+                    ]);
+                $("#addRowModal").modal("hide");
+            });
         });
-      </script>
+    </script>
 
-  </body>
+<script>
+    $(document).ready(function(){
+
+        $image_crop = $('#image_demo').croppie({
+            enableExif: true,
+            viewport: {
+                width:300,
+                height:300,
+                type:'square' //circle
+            },
+            boundary:{
+                width:400,
+                height:500
+            }
+        });
+        $('#before_crop_image').on('change', function(){
+            var reader = new FileReader();
+            reader.onload = function (event) {
+                $image_crop.croppie('bind', {
+                    url: event.target.result
+                }).then(function(){
+                    console.log('jQuery bind complete');
+                });
+            }
+            reader.readAsDataURL(this.files[0]);
+            $('#imageModel').modal('show');
+
+        });
+        $('.crop_image').click(function(event){
+            alert();
+        });
+    });
+    </script>
+
+</body>
+
 </html>
--- metahome/resources/views/admin/pages/account/edit.blade.php
+++ metahome/resources/views/admin/pages/account/edit.blade.php
@@ -8,72 +8,40 @@
                 </div>
             </div>
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-md-4">
                     <div class="card">
                         <div class="card-body">
-                            <div class="col-md-6 ms-auto me-auto">
-                                <div class="text-center">
-                                    <div class="avatar avatar-xxl">
-                                        <img src="../assets/img/jm_denis.jpg" alt="..."
-                                            class="avatar-img rounded-circle">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-6 ms-auto me-auto">
-                                <div class="text-center">
-                                    <p class="pt-2 h3">Nguyen Ngoc Duc</p>
-                                    <p class="pt-1 h5">Nam</p>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-4 col-lg-4">
-                                    <div class="form-group">
-                                        <label for="email">Email</label>
-                                        <input type="text" class="form-control" id="email"
-                                            value="ngocduc.bk.hust@gmail.com" disabled>
-                                    </div>
-                                </div>
-                                <div class="col-md-4 col-lg-4">
-                                    <div class="form-group">
-                                        <label for="phone">Phone</label>
-                                        <input type="text" class="form-control" id="phone" value="0378049798"
-                                            disabled>
-                                    </div>
-                                </div>
-                                <div class="col-md-4 col-lg-4">
-                                    <div class="form-group">
-                                        <label for="CCCD">CCCD</label>
-                                        <input type="text" class="form-control" id="CCCD" value="038099013860"
-                                            disabled>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="row">
-                                <div class="col-md-2 col-lg-2">
-                                    <div class="form-group">
-                                        <label for="dob">Date of Birth</label>
-                                        <input type="text" class="form-control" id="dob" value="16/05/1999"
-                                            disabled>
-                                    </div>
-                                </div>
-                                <div class="col-md-2 col-lg-2">
-                                    <div class="form-group">
-                                        <label for="role">Role</label>
-                                        <input type="text" class="form-control" id="role" value="Admin" disabled>
-                                    </div>
-                                </div>
-                                <div class="col-md-8 col-lg-8">
-                                    <div class="form-group">
-                                        <label for="address">Address</label>
-                                        <input type="text" class="form-control" id="address"
-                                            value="Thon 4, Quang Giao, Quang Xuong, Thanh Hoa" disabled>
-                                    </div>
-                                </div>
-                            </div>
+                            <input type="file" name="before_crop_image" id="before_crop_image" accept="image/*">
+
+
+
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="card">
+                        <div class="card-body">
+
                         </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
+    <!-- Modal -->
+    <div id="imageModel" class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Crop and Resize Avatar</h5>
+            </div>
+            <div class="modal-body">
+            <div id="image_demo" style="width:350px; margin-top:30px"></div>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary crop_image">Crop</button>
+            </div>
+          </div>
+        </div>
+      </div>
 @endsection
